25358e2e9e70b38c1b99cfe2d5483472882eb798
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-08-26 16:59:10 -0400

    Added pics and adjusted forms to accept inputs

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
- // Your web app's Firebase configuration
- var firebaseConfig = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyCeA5viJaX25v4XjKToC-AtbpGdXd9YjL0",
     authDomain: "train-time-364c1.firebaseapp.com",
     databaseURL: "https://train-time-364c1.firebaseio.com",
@@ -7,38 +7,50 @@
     storageBucket: "",
     messagingSenderId: "70634217960",
     appId: "1:70634217960:web:b48e3020ac3d6fa8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  // Assumptions
-  var tFrequency = 3;
+// Assumptions
+var tFrequency = 3;
 
-  // Time is 3:30 AM
-  var firstTime = "03:30";
+// Time is 3:30 AM
+var firstTime = "03:30";
 
-  // First Time (pushed back 1 year to make sure it comes before current time)
-  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-  console.log(firstTimeConverted);
+// First Time (pushed back 1 year to make sure it comes before current time)
+var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+console.log(firstTimeConverted);
 
-  // Current Time
-  var currentTime = moment();
-  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+// Current Time
+var currentTime = moment();
+console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-  // Difference between the times
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  console.log("DIFFERENCE IN TIME: " + diffTime);
+// Difference between the times
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+console.log("DIFFERENCE IN TIME: " + diffTime);
 
-  // Time apart (remainder)
-  var tRemainder = diffTime % tFrequency;
-  console.log(tRemainder);
+// Time apart (remainder)
+var tRemainder = diffTime % tFrequency;
+console.log(tRemainder);
 
-  // Minute Until Train
-  var tMinutesTillTrain = tFrequency - tRemainder;
-  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+// Minute Until Train
+var tMinutesTillTrain = tFrequency - tRemainder;
+console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-  // Next Train
-  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+// Next Train
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+//   INSERT FUNCTION TO GRAB DATA, CALCULATE, ARE REPOPULATE TABLE
+
+
+// INSERT MOMENT FUNCTION TO GET TIMES
+var randomDate = "02/23/1999";
+var randomFormat = "MM/DD/YYYY";
+var convertedDate = moment(randomDate, randomFormat);
+
+convertedDate.format("MMM Do, YYYY hh:mm:ss"));
+moment().endOf('day').fromNow();          // in an hour
+moment().startOf('hour').fromNow();      
