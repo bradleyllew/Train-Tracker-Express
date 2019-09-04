cbf128f35a64599c54dc965cd04d92bf176316cc
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-09-03 23:11:53 -0400

    trying 2 different ways. Neither working? Sched2 posting to DOM tho, but not taking current time into account?

--- assets/app.js
+++ assets/app.js
@@ -1,56 +1,106 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCeA5viJaX25v4XjKToC-AtbpGdXd9YjL0",
-    authDomain: "train-time-364c1.firebaseapp.com",
-    databaseURL: "https://train-time-364c1.firebaseio.com",
-    projectId: "train-time-364c1",
-    storageBucket: "",
-    messagingSenderId: "70634217960",
-    appId: "1:70634217960:web:b48e3020ac3d6fa8"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var database = firebase.database();
-
-// Assumptions
-var tFrequency = 3;
-
-// Time is 3:30 AM
-var firstTime = "03:30";
-
-// First Time (pushed back 1 year to make sure it comes before current time)
-var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-console.log(firstTimeConverted);
-
-// Current Time
-var currentTime = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-// Difference between the times
-var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-console.log("DIFFERENCE IN TIME: " + diffTime);
-
-// Time apart (remainder)
-var tRemainder = diffTime % tFrequency;
-console.log(tRemainder);
-
-// Minute Until Train
-var tMinutesTillTrain = tFrequency - tRemainder;
-console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-// Next Train
-var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-//   INSERT FUNCTION TO GRAB DATA, CALCULATE, ARE REPOPULATE TABLE
+$(document).ready(function () {
 
 
-// INSERT MOMENT FUNCTION TO GET TIMES
-var randomDate = "02/23/1999";
-var randomFormat = "MM/DD/YYYY";
-var convertedDate = moment(randomDate, randomFormat);
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCeA5viJaX25v4XjKToC-AtbpGdXd9YjL0",
+        authDomain: "train-time-364c1.firebaseapp.com",
+        databaseURL: "https://train-time-364c1.firebaseio.com",
+        projectId: "train-time-364c1",
+        storageBucket: "",
+        messagingSenderId: "70634217960",
+        appId: "1:70634217960:web:b48e3020ac3d6fa8"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    // To call the database
+    var database = firebase.database();
 
-convertedDate.format("MMM Do, YYYY hh:mm:ss"));
-moment().endOf('day').fromNow();          // in an hour
-moment().startOf('hour').fromNow();      
+    var whichTrain = $("#option").val();
+    var destination = $("#dest").val();
+    var firstTrain = moment($("#firstTrain").val(), "HH:mm").subtract(1, "years").format("X");
+    var tFrequency = $("#frqncy").val();
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(firstTrain), "minutes");
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+
+    // Submit button event listener
+    $("#submitBtn").on("click", function (event) {
+        // Bez we were told to...keeps user from repeatidly pressing button?
+        event.preventDefault();
+
+
+        // Which train user selects
+        var whichTrain = $("#option").val();
+        console.log("WHICH TRAIN: " + whichTrain);
+
+        // Destination
+        var destination = $("#dest").val();
+        console.log("Destination " + destination);
+
+        // First Train (pushed back 1 year to make sure it comes before current time)
+        var firstTrain = moment($("#firstTrain").val(), "HH:mm").subtract(1, "years").format("X");
+        console.log("FIRST TRAIN: " + firstTrain);
+
+        var tFrequency = $("#frqncy").val();
+
+        //Current time
+        var currentTime = moment();
+        console.log("TIME NOW: " + moment(currentTime).format("HH:mm"));
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTrain), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % tFrequency;
+        console.log(tRemainder);
+
+        // Minute Until Train
+        var tMinutesTillTrain = tFrequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+
+
+        var option = {
+            train: whichTrain,
+            destination: destination,
+            frequency: tFrequency
+        }
+
+        //To add tain to Firebase
+        database.ref().push(option);
+
+        // To add values to table - NOT WORKING
+        $("#frqncy").val();
+        $("#next").val();
+        $("#mins").val();
+
+        database.ref().on("child_added", function (childSnapshot) {
+            // Log everything that's coming out of snapshot
+
+            console.log(childSnapshot.val());
+            console.log(childSnapshot.val().whichTrain);
+            console.log(childSnapshot.val().tFrequency);
+            console.log(childSnapshot.val().nextTrain);
+            console.log(childSnapshot.val().tMinutesTillTrain);
+            // Change the HTML to reflect
+            $("#frqncy").text(childSnapshot.val().tFrequency);
+            $("#next").text(childSnapshot.val().nextTrain);
+            $("#mins").text(childSnapshot.val().tMinutesTillTrain);
+
+
+            // Handle the errors
+        }, function (errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+        });
+
+
+    });
+
+})
--- assets/app2.js
+++ assets/app2.js
@@ -1,1 +1,70 @@
-CCC
+$(document).ready(function () {
+
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCeA5viJaX25v4XjKToC-AtbpGdXd9YjL0",
+        authDomain: "train-time-364c1.firebaseapp.com",
+        databaseURL: "https://train-time-364c1.firebaseio.com",
+        projectId: "train-time-364c1",
+        storageBucket: "",
+        messagingSenderId: "70634217960",
+        appId: "1:70634217960:web:b48e3020ac3d6fa8"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    // To call the database
+    var database = firebase.database();
+
+    // Variables for the Event Listener
+    var name;
+    var firstTrain;
+    var frequency = 0;
+
+
+
+    $("#submitBtn").on("click", function () {
+        event.preventDefault();
+        // Storing and retreiving new train data
+        name = $("#option").val().trim();
+        console.log("NAME " + name);
+        firstTrain = $("#firstTrain").val().trim();
+        console.log("FIRST TRAIN " + firstTrain);
+        frequency = $("#frequency").val();
+        console.log("FREQ " + frequency);
+
+        // Pushing to database
+        database.ref().push({
+            name: name,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        $("form")[0].reset();
+    });
+
+    database.ref().on("child_added", function (childSnapshot) {
+        // pushed back 1 year to make sure it comes before current time
+        var firstTrainNew = moment(childSnapshot.val().firstTrain, "HH:mm").subtract(1, "years");
+        // Difference between the times/time apart=remainder
+        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+        var remainder = diffTime % childSnapshot.val().frequency;
+        // Minutes until next train
+        var minAway = childSnapshot.val().frequency - remainder;
+        // Next train time
+        var nextTrain = moment().add(minAway, "minutes");
+        nextTrain = moment(nextTrain).format("hh:mm");
+
+        $("#add-row").append("<tr><td>" + childSnapshot.val().name +
+            "</td><td>" + childSnapshot.val().destination +
+            "</td><td>" + childSnapshot.val().frequency +
+            "</td><td>" + nextTrain +
+            "</td><td>" + minAway + "</td></tr>");
+
+        // Handle the errors
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+});
+
+
